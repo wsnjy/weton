6499ffc5697199f80e43d65827d5f87212ef79a2
wisnu <wisnu@sebangsa.com>
2015-06-06 17:08:41 +0700

    Initial Commit

---
+++ WETON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WETON
+//
+//  Created by Wisnu Sanjaya on 6/6/15.
+//  Copyright (c) 2015 wsnjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WETON/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WETON
+//
+//  Created by Wisnu Sanjaya on 6/6/15.
+//  Copyright (c) 2015 wsnjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
