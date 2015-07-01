3885181ea256b4f30a7155af5686bcf0feab7907
wisnu <wisnu@sebangsa.com>
2015-07-01 18:08:23 +0700

    commit halaman result fix

---
+++ WETON/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  WETON
+//
+//  Created by Wisnu Sanjaya on 7/1/15.
+//  Copyright (c) 2015 wsnjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
